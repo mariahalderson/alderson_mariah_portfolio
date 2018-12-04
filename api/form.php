9e5087d251b3efb5160934a62ff4b5606ad6579a
Mariah Alderson <mariah.alderson@gmail.com>
2018-12-04 16:42:03 -0500

    mail functions

---
+++ api/form.php
@@ -0,0 +1,61 @@
+<?php
+
+function send_email() {
+
+    $name = 'name: '.$_POST['name'];
+    $email = 'from: '.$_POST['email'];
+    $subject = 'subject: '.$_POST['subject'];
+    $message = 'message: '.$_POST['message'];
+
+    $to = 'mariah.alderson@gmail.com';
+    $headers = 'From: noreply@mariahalderson.com' . '\r\n';
+    $headers .= "Reply-To:".$_POST['email'];
+
+    //mail($to, $subject, $message, $headers);
+
+    if(isset ($to, $subject, $message, $headers)){
+        echo "everything set!" . '\r\n'. $to . '\r\n' . $subject . '\r\n' . $message . '\r\n' . $headers;
+    }else{
+        echo "we are missing one";
+    }
+    //eth
+    // if(empty($_POST['email'])){
+    //     header('Location:/index.php?status=error');
+    //     exit();
+    // }
+    //if fields are not filled, redirect to form with no success message
+    // if(!isset($_POST['message']) || !isset($_POST['email'])){
+    //     header('Location:/forms/index.php');
+    //     exit();
+    // }
+
+//   $to = 'mariah.alderson@gmail.com';
+//   $subject = 'Email from Week 12';
+//   $message = 'Message Body:'.$_POST['message'];
+//   $headers = 'From: noreply@mariahalderson.com' . '\r\n'; //the \r\n is a line break
+//   $headers .= "Reply-To:".$_POST['email'];
+
+  //use this function on host to send message
+  //mail($to, $subject, $message, $headers);
+
+  //success status for index.php
+//   if(isset ($to, $subject, $message, $headers)){
+//     header('Location:/forms/index.php?status=success');
+// }
+
+//use this if instead of above when on host 
+//   if(mail($to, $subject, $message, $headers)){
+//       header('Location:./forms/index.php?status=success');
+//   }
+    
+    //header('Location: ./form.php');
+
+
+}
+
+send_email();
+//echo "name: ".$_POST['name'];
+header("Access-Control-Allow-Origin: *");
+ header('Content-Type:application/json;charset=UTF-8');
+
+?>
--- src/Contact.vue
+++ src/Contact.vue
@@ -9,18 +9,19 @@
             <p class="copy">Feel free to contact me here for inquiries at any time, and I will be sure to get back to you as soon as possible.</p>
 
             <div id="formcontainer">
-                <form action="./api/form.php" method="POST">
+                <!-- <form action="./api/form.php" method="POST"> -->
+                <form action="http://localhost/alderson_mariah_portfolio/api/form.php" method="POST">
                     <label for="name" type="text">name:</label>
-                    <input name="name" class="formfield" autofocus>
+                    <input name="name" class="formfield" v-model="form.name">
 
                     <label for="email" type="email">email:</label>
-                    <input name="email" class="formfield">
+                    <input name="email" class="formfield" v-model="form.email">
 
                     <label for="subject" type="text">subject:</label>
-                    <input name="subject" class="formfield">
+                    <input name="subject" class="formfield" v-model="form.subject">
 
                     <label for="message" type="text">message:</label>
-                    <textarea name="message" class="formfield"></textarea>
+                    <textarea name="message" class="formfield" v-model="form.message"></textarea>
                     <button type="submit" class="button">submit</button>
                 </form>
             </div>
@@ -33,12 +34,23 @@
 
 
 <script>
-    //  export default{
-    //      mounted(){
-    //     //fix header styling
-    //     this.$refs.firstheader.$refs.homeheader.classList.remove("fullscreen");
-    //     this.$refs.firstheader.$refs.layer.style.width="100vw";
-    //     this.$refs.firstheader.$refs.logobox.style.width="90px";
-    //     }
-    //  }
+     export default{
+        data(){
+            return{
+                form:{
+                    name: "",
+                    email: "",
+                    subject: "",
+                    message: ""
+                }
+            }
+        },
+
+        methods: {
+            submithandle(){
+                console.log(this.form.name + this.form.email + this.form.subject + this.form.message);
+            }
+        }
+    }
+     
 </script>
